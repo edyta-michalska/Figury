74be1530586fb6b8b86048da3ba89bcda1e0ef4e
edyta-michalska <e.michalska@microsoft.wsei.edu.pl>
2020-03-02 20:27:37 +0100

    nowości

---
+++ Figury/ConsoleAppFigury/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FiguryLibrary;
+
+namespace ConsoleAppFigury
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trojkat t1; //deklaracja zmiennej typu trojkat
+            t1 = new Trojkat();
+            Trojkat t2 = new Trojkat(1, 2, 3);
+            var t3 = new Trojkat(c: 1, b: 2, a: 2);
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t1.GetObwod());
+            Console.WriteLine(t1.Obwod);
+            Console.WriteLine($"pole = { t1.GetPole()}");
+            Console.WriteLine($"pole = {t1.Pole}");
+            try
+            {
+                t1.A = -1;
+                t1.B = -2;
+                t1.C = -3;
+            }
+            catch(ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("nie wolno bokom przypisywać wartosci ujemnych");
+            }
+            
+            Console.WriteLine(t1);
+            Console.WriteLine(t1.Pole);
+
+            //Console.WriteLine(t2);
+            //Console.WriteLine(t3);
+
+        }
+       
+
+    }
+}
---
+++ Figury/FiguryLibrary/Trojkat.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace FiguryLibrary
+{
+    public class Trojkat
+    {
+        //dane obiektu - pola obiektu (fields)
+        private double a; //this.a
+
+        public double A
+        {
+            get { return a; }
+            set
+            { if (value <= 0)
+                    throw new ArgumentOutOfRangeException("wartość A musi być dodatnia");
+                else
+                    a = value;
+            }
+
+        }
+        private double b;// this.b
+
+        public double B
+        {
+            get => b;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("wartość B musi być dodatnia");
+                else
+                    b = value;
+            }
+        }
+        private double c; //this.c
+        public double C
+        {
+            get => c;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("wartość C musi być dodatnia");
+                else
+                    c = value;
+
+            }
+        }
+
+
+
+        //konstruktory
+        public Trojkat()  //konstruktor domyslny
+        {
+            a = b = c = 1.0;
+        }
+
+        public Trojkat(double a, double b, double c)
+        {
+
+            if (a + b <= c || a + c <= b || b + c <= a)
+            {
+                throw new ArgumentException("mie spełniony warunek trojkata");
+            }
+
+            this.A = a;
+            this.B = b;
+            this.C = c;
+
+
+        }
+        //testowa reprezentacja obiektu
+        public override string ToString()
+        {
+            return $"trójkąt(a: {a},b: {b},c: {c})";
+        }
+
+        //wlasciwosci i metody
+
+        public double GetObwod() //metoda zwracajaca obwod tego trojkata
+        {
+            return a + b + c;
+        }
+        //property zwracajace obwod tego trokata
+        public double Obwod => a + b + c;
+
+        public double GetPole() //jawa style
+        {
+            var p = 0.5 * GetObwod();
+            var s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return s;
+
+
+        }
+        public double Pole
+        {
+            get
+            {
+                var p = 0.5 * Obwod;
+                var s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                return s;
+
+            }
+
+
+        }
+    }
+    
+}
